770cd4fee8f39e2fde44ab0d22027dd7d4b6d42e
RAJ Heman <heman.raj@soprasteria.com>
2022-06-16 19:18:57 +0530

    ncom6

--- base/views.py
+++ base/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required(login_url='/loginpage')
+@login_required(login_url='/authendicate/loginpage')
 def home(request):
     return render(request,'base/home.html')
